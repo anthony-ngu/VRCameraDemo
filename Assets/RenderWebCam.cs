5ff05e85ba1912bf841f6670876ff152d55a4954
Anthony Ngu <anthony.ngu@outlook.com>
2015-09-06 14:32:19 -0700

    Added the Unity project

---
+++ Assets/RenderWebCam.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderWebCam : MonoBehaviour {
+
+	public string deviceName;
+	private WebCamTexture webcamTexture;
+	private Quaternion baseRotation;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+		// Ensure orientation and scaling of the image.		
+		var scale = 2.2f;
+		Quaternion rotation = Quaternion.Euler(0, 0, 0);
+		Matrix4x4 rotationMatrix = Matrix4x4.TRS(Vector3.zero, rotation, new Vector3(scale , scale , scale));
+		gameObject.GetComponent<Renderer>().material.SetMatrix("_Rotation", rotationMatrix);
+
+		int t = 0;
+		bool front_facing = false;
+
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		// Gets the back facing camera if there is one on the device
+		while(front_facing && t < devices.Length){
+			deviceName = devices[t].name;
+			front_facing = devices[t].isFrontFacing;
+			t++;
+		}
+
+		webcamTexture = new WebCamTexture(deviceName);
+
+		// performs the vertical axis flip
+		if (!front_facing) {
+			transform.Rotate(new Vector3(0,0,180));
+		}
+
+		// performs the horizontal axis flip that is the issue regardless
+		transform.localScale = new Vector3(-1 * transform.localScale.x, 
+		                                   transform.localScale.y,
+		                                   transform.localScale.z);
+
+		GetComponent<Renderer>().material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/SocketClient.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class SocketClient : MonoBehaviour {
+	public string server = "127.0.0.1";
+	public int port = 6969;
+	private static Socket ConnectSocket(string server, int port)
+	{
+		Socket s = null;
+		IPHostEntry hostEntry = null;
+
+		// Get host related information.
+		hostEntry = Dns.GetHostEntry(server);
+
+		// Loop through the AddressList to obtain the supported AddressFamily. This is to avoid 
+		// an exception that occurs when the host IP Address is not compatible with the address family 
+		// (typical in the IPv6 case). 
+		foreach(IPAddress address in hostEntry.AddressList)
+		{
+			IPEndPoint ipe = new IPEndPoint(address, port);
+			Socket tempSocket = 
+				new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			
+			tempSocket.Connect(ipe);
+			
+			if(tempSocket.Connected)
+			{
+				s = tempSocket;
+				break;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		return s;
+	}
+
+	// This method requests the home page content for the specified server. 
+	private static string SocketSendReceive(string server, int port) 
+	{
+		// Create a socket connection with the specified server and port.
+		Socket s = ConnectSocket(server, port);
+		
+		if (s == null)
+			return ("Connection failed");
+		
+		// Send request to the server.
+		s.Send(Encoding.ASCII.GetBytes("a"));  
+		
+		return "";
+	}
+
+	// Use this for initialization
+	void Start () {
+		string result = SocketSendReceive(server, port); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
